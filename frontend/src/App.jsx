b4d2ec27e5d2cb9e61053fee97900dbdf1d39912
Mansi-kadam-1706 <kadammansi1706@gmail.com>
2026-01-30 23:11:33 +0530

    Fix BrowserRouter basename and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Signup from "./pages/Signup"; 
+import Signup from "./pages/Signup";
 import StudentDashboard from "./pages/StudentDashboard";
 import TeacherDashboard from "./pages/TeacherDashboard";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login/>}/>
-      <Route path="/signup" element={<Signup/>} />
-      <Route path="/student" element={<StudentDashboard/>} />
-      <Route path="/teacher" element={<TeacherDashboard />} />
-    </Routes>
+    <BrowserRouter basename="/ATTENDANCE-MANAGER">
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/student" element={<StudentDashboard />} />
+        <Route path="/teacher" element={<TeacherDashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
